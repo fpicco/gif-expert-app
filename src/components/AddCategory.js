63d3a6fdeb692a1414efd8e0cf32b009ba280792
fpicco <flor.picco@gmail.com>
2021-11-23 16:11:04 -0300

    GifExpertApp baseline

---
+++ src/components/AddCategory.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
+export const AddCategory = ({ setCategories }) => {
+  const [inputValue, setInputValue] = useState("");
+  const handdleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handdleSubmit = (e) => {
+    e.preventDefault();
+
+    if (inputValue.trim().length > 2) {
+      setCategories((categ) => [inputValue, ...categ]);
+      setInputValue("");
+    }
+  };
+
+  return (
+    <form onSubmit={handdleSubmit}>
+      <input type="text" value={inputValue} onChange={handdleInputChange} />
+    </form>
+  );
+};
+
+AddCategory.propTypes = {
+  setCategories: PropTypes.func.isRequired,
+};
